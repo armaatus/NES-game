631d90e8b0458fcfc1231c8c82f079d1a5805a8a
Joris de Jong <j.h.c.dejong@students.uu.nl>
2025-05-25 00:17:43 +0200

    changed folder name and removed chr files that are no longer used

--- compile.bat
+++ compile.bat
@@ -1,11 +1,11 @@
-..\cc65\bin\ca65 src\tutorial.s
+..\cc65\bin\ca65 src\main.s
 ..\cc65\bin\ca65 src\reset.s 
 ..\cc65\bin\ca65 src\controllers.s
 ..\cc65\bin\ca65 src\background.s
 
-..\cc65\bin\ld65 src\reset.o src\background.o src\controllers.o src\tutorial.o -C nes.cfg -o tutorial.nes
+..\cc65\bin\ld65 src\reset.o src\background.o src\controllers.o src\main.o -C nes.cfg -o game.nes
 
-del src\tutorial.o
+del src\main.o
 del src\reset.o
 del src\controllers.o
 del src\background.o
