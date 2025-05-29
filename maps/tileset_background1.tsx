3998058d6eb4d2280a8529ae63f031eb7b8b63fc
Joris de Jong <j.h.c.dejong@students.uu.nl>
2025-05-29 23:23:32 +0200

    new assets, made start to working with metatiles in Tiled and added a webpage to convert images to something than maybe can work in chr convertion

--- maps/tileset_background1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="background1" tilewidth="8" tileheight="8" tilecount="512" columns="16">
- <image source="Tileset_sources/background1.png" width="128" height="256"/>
-</tileset>
