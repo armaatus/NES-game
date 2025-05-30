c0e31170e92cca5e110acd1638ef7685f4f01a84
Joris de Jong <j.h.c.dejong@students.uu.nl>
2025-05-30 23:03:04 +0200

    added RLE compression to Tiled asm generation extension

--- utils/Tiled/Tiled2Asm.js
+++ utils/Tiled/Tiled2Asm.js
@@ -3,7 +3,7 @@ This extension exports Tiled map level data as Assembly (6502) data format in si
 later that can be imported in your project directly by including that file.
 Extension : Export Assembly data. 
 Author: Joris de Jong
-Tool: Tiled (1.11.2)
+Tool: Tiled (1.11.5)
 Copyleft@ 2025
 
 Usage: 
@@ -17,8 +17,9 @@ macOS       ~/Library/Preferences/Tiled/extensions/
 Linux       ~/.config/tiled/extensions/
 */
 
-const NAMETABLEWIDTH = 32;
-const NAMETABLEHEIGHT = 30;
+const NAMETABLE_WIDTH = 32;
+const NAMETABLE_HEIGHT = 30;
+const RLE_ENABLED = true;
 
 //Method to export multi-map (Map with multiple rooms)
 function multiMapExportAsm(filePath, map) {
@@ -44,8 +45,8 @@ function multiMapExportAsm(filePath, map) {
   file.write(exportHeader);
 
   // Get the amount of rooms
-  var hLoop = map.width / NAMETABLEWIDTH;
-  var vLoop = map.height / NAMETABLEHEIGHT;
+  var hLoop = map.width / NAMETABLE_WIDTH;
+  var vLoop = map.height / NAMETABLE_HEIGHT;
   var loops = vLoop == 1 && hLoop == 1 ? 1 : vLoop + hLoop;
 
   //Writing export data.
@@ -56,14 +57,24 @@ function multiMapExportAsm(filePath, map) {
         file.write(fileName + "_" + room + ":\n.byte ");
         for (var v = 0; v < vLoop; v++) {
           for (let h = 0; h < hLoop; h++) {
-            for (let y = 0; y < NAMETABLEHEIGHT; y++) {
-              for (let x = 0; x < NAMETABLEWIDTH; x++) {
-                currentX = h * NAMETABLEWIDTH + x;
-                currentY = v * NAMETABLEHEIGHT + y;
-                file.write("$" + layer.cellAt(currentX, currentY).tileId);
-                file.write(x < NAMETABLEWIDTH - 1 ? "," : "");
+            for (let y = 0; y < NAMETABLE_HEIGHT; y++) {
+              let tilesOnX = [];
+              for (let x = 0; x < NAMETABLE_WIDTH; x++) {
+                currentX = h * NAMETABLE_WIDTH + x;
+                currentY = v * NAMETABLE_HEIGHT + y;
+                let currentTileId = layer.cellAt(currentX, currentY).tileId;
+                if (RLE_ENABLED) {
+                  // Use RLE compression
+                  tilesOnX.push(currentTileId);
+                } else {
+                  // Just print every tile on each location
+                  file.write(`${decToHex(currentTileId)}`);
+                  file.write(x < NAMETABLE_WIDTH - 1 ? "," : "");
+                }
               }
-              if (y != NAMETABLEHEIGHT - 1) {
+              if (RLE_ENABLED) {
+                RLECompression(tilesOnX, file, y);
+              } else if (y != NAMETABLE_HEIGHT - 1) {
                 file.write("\n.byte ");
               }
             }
@@ -85,6 +96,47 @@ function multiMapExportAsm(filePath, map) {
   file.commit();
 }
 
+// RLE
+function RLECompression(values, file, y) {
+  // Catch if values has length of one
+  if (values.length == 1) {
+    file.write(`$01,$${decToHex(values[0])}`);
+    return;
+  }
+
+  let currentValue = values[0];
+  let currentCount = 1;
+  for (let i = 1; i < values.length; i++) {
+    const element = values[i];
+
+    if (currentValue != element) {
+      file.write(`${decToHex(currentCount)},`);
+      file.write(`${decToHex(currentValue)},  `);
+
+      // Set it to the new values
+      currentCount = 0;
+      currentValue = element;
+    }
+
+    // End of the list catch
+    if (i == values.length - 1) {
+      currentCount++;
+      file.write(`${decToHex(currentCount)},`);
+      file.write(`${decToHex(currentValue)}`);
+      if (y != NAMETABLE_HEIGHT - 1) {
+        file.write("\n.byte ");
+      }
+      return;
+    }
+
+    currentCount++;
+  }
+}
+
+function decToHex(number) {
+  return `$${number < 16 ? "0" : ""}${number.toString(16)}`;
+}
+
 //Main Export method.
 var asmExportFormat = {
   name: "Assembly format (6502)",
@@ -92,13 +144,13 @@ var asmExportFormat = {
 
   write: function (map, filePath) {
     //Check map properties.
-    if (map.width < 16) {
+    if (map.width < 32) {
       tiled.alert("File too small to handle.", "Export error");
       return;
     }
 
-    if ((map.width & 0x0f) != 0)
-      tiled.alert("Warning expected width divisible by 16.");
+    if ((map.width & 0x1f) != 0)
+      tiled.alert("Warning expected width divisible by 32.");
 
     multiMapExportAsm(filePath, map);
   },
